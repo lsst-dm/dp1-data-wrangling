a4e3f385afcde99fa7d71b957681822b39342d0e
David H. Irving <david.irving@noirlab.edu>
2025-01-28 15:41:41 -0800

    Dump dataset information

--- python/lsst/dp1_data_wrangling/export-preliminary-dp1.py
+++ python/lsst/dp1_data_wrangling/export-preliminary-dp1.py
@@ -1,23 +1,26 @@
-from collections.abc import Iterable
-from contextlib import AbstractContextManager
 import pathlib
 
-from lsst.daf.butler import Butler, DimensionRecordTable, DimensionElement, DimensionRecord, DatasetRef, DatasetType
+from lsst.daf.butler import Butler, DimensionRecordTable, DimensionElement, DimensionRecord, DatasetRef, DatasetType, DimensionGroup
+import pyarrow
 from pyarrow.parquet import ParquetWriter
 
 
 COLLECTIONS = [ "LSSTComCam/runs/DRP/DP1/w_2025_03/DM-48478"]
 OUTPUT_DIRECTORY = "dp1-dump-test"
+DIMENSION_SUBDIRECTORY = "dimensions"
+DATASETS_SUBDIRECTORY = "datasets"
+MAX_ROWS_PER_WRITE = 100000
 
 def main() -> None:
     output_path = pathlib.Path(OUTPUT_DIRECTORY)
     output_path.mkdir(parents=True, exist_ok=True)
 
     butler = Butler("/repo/main")
-    refs = butler.query_datasets("calexp", COLLECTIONS, find_first=False, with_dimension_records=True)
-
     dumper = DatasetsDumper(output_path)
-    dumper.write_refs(refs)
+
+    with butler.registry.caching_context():
+        dumper.dump_refs(butler, "calexp")
+
     dumper.finish()
 
 class DatasetsDumper:
@@ -27,10 +30,28 @@ class DatasetsDumper:
         self._dimensions: dict[str, DimensionRecordParquetWriter] = {}
         self._output_path = output_path
 
-    def write_refs(self, refs: Iterable[DatasetRef]) -> None:
-        for ref in refs:
-            for key, record in ref.dataId.records.items():
-                self._add_dimension_record(key, record)
+        dimension_output_directory = output_path.joinpath(DIMENSION_SUBDIRECTORY)
+        dimension_output_directory.mkdir(exist_ok=True)
+        self._dimension_output_directory = dimension_output_directory
+
+        dataset_output_directory = output_path.joinpath(DATASETS_SUBDIRECTORY)
+        dataset_output_directory.mkdir(exist_ok=True)
+        self._dataset_output_directory = dataset_output_directory
+
+        self._dataset_types_written: set[str] = set()
+
+    def dump_refs(self, butler: Butler, dataset_type_name: str) -> None:
+        assert dataset_type_name not in self._dataset_types_written, "Each dataset type must be written only once"
+        self._dataset_types_written.add(dataset_type_name)
+
+        dataset_type = butler.get_dataset_type(dataset_type_name)
+        writer = DatasetsParquetWriter(dataset_type, str(self._dataset_output_directory.joinpath(f"{dataset_type_name}.parquet")))
+        with butler.query() as query:
+            for ref in query.datasets(dataset_type, COLLECTIONS, find_first=False).with_dimension_records():
+                writer.add_ref(ref)
+                for key, record in ref.dataId.records.items():
+                    self._add_dimension_record(key, record)
+            writer.finish()
 
     def finish(self) -> None:
         for writer in self._dimensions.values():
@@ -42,7 +63,7 @@ class DatasetsDumper:
 
         writer = self._dimensions.get(dimension)
         if writer is None:
-            writer = DimensionRecordParquetWriter(record.definition, str(self._output_path.joinpath(f"{dimension}.parquet")))
+            writer = DimensionRecordParquetWriter(record.definition, str(self._dimension_output_directory.joinpath(f"{dimension}.parquet")))
             self._dimensions[dimension] = writer
         writer.add_record(record)
 
@@ -55,7 +76,7 @@ class DimensionRecordParquetWriter:
 
     def add_record(self, record: DimensionRecord) -> None:
         self._records.append(record)
-        if len(self._records) > 100000:
+        if len(self._records) >= MAX_ROWS_PER_WRITE:
             self._flush_records()
 
     def _flush_records(self) -> None:
@@ -66,6 +87,49 @@ class DimensionRecordParquetWriter:
     def finish(self) -> None:
         self._flush_records()
         self._writer.close()
+
+class DatasetsParquetWriter:
+    def __init__(self, dataset_type: DatasetType, output_file: str) -> None:
+        self._schema = create_dataset_arrow_schema(dataset_type)
+        self._writer = ParquetWriter(output_file, self._schema)
+        self._rows: list[dict] = []
+
+    def add_ref(self, ref: DatasetRef) -> None:
+        row = dict(ref.dataId.required)
+        row["dataset_id"] = ref.id.bytes
+        row["run"] = ref.run
+
+        self._rows.append(row)
+        if len(self._rows) >= MAX_ROWS_PER_WRITE:
+            self._flush_rows()
+
+    def _flush_rows(self) -> None:
+        batch = pyarrow.RecordBatch.from_pylist(self._rows, schema=self._schema)
+        self._writer.write(batch)
+        self._rows.clear()
+
+    def finish(self) -> None:
+        self._flush_rows()
+        self._writer.close()
+
+
+def create_dataset_arrow_schema(dataset_type: DatasetType) -> pyarrow.Schema:
+    fields = [
+        pyarrow.field("dataset_id", pyarrow.binary(16)),
+        pyarrow.field("run", pyarrow.string()),
+        *_get_data_id_column_schemas(dataset_type.dimensions),
+    ]
+    print(fields)
+    return pyarrow.schema(fields)
+
+def _get_data_id_column_schemas(dimensions: DimensionGroup) -> list[pyarrow.Field]:
+    schema = []
+    for dimension in dimensions.required:
+        dimension = dimensions.universe.dimensions[dimension]
+        field = pyarrow.field(dimension.name, dimension.primary_key.to_arrow().data_type)
+        schema.append(field)
+
+    return schema
 
 if __name__ == "__main__":
     main()
