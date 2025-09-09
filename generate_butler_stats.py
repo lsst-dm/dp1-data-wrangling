b661523cb42957689389ce762a78ccb745747842
David H. Irving <david.irving@noirlab.edu>
2025-09-09 15:52:33 -0700

    Correlate dataset types with IDs

--- generate_butler_stats.py
+++ generate_butler_stats.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 
+import os
 import re
 import uuid
 from collections import Counter
-from collections.abc import Iterator
+from collections.abc import Iterable, Iterator
+from glob import glob
 from typing import Literal, NamedTuple
+
+from pyarrow.parquet import ParquetFile
 
 
 def main() -> None:
@@ -12,6 +16,14 @@ def main() -> None:
     by_endpoint = Counter(access.endpoint for access in accesses)
     print(f"Total: {len(accesses)}")
     print(by_endpoint)
+    by_dataset_id = Counter(access.id for access in accesses if access.id is not None)
+    print(f"Unique datasets accessed: {len(by_dataset_id)}")
+
+    dataset_type_mapping = find_dataset_types(by_dataset_id.keys())
+    by_dataset_type = Counter[str]()
+    for id, dataset_type in dataset_type_mapping.items():
+        by_dataset_type[dataset_type] += by_dataset_id[id]
+    print(by_dataset_type)
 
 
 def find_file_accesses() -> Iterator[FileAccess]:
@@ -31,6 +43,26 @@ def find_file_accesses() -> Iterator[FileAccess]:
                 yield FileAccess(repo=repo, id=id, endpoint="download")
             else:
                 raise ValueError(f"Unhandled log line: {rest}")
+
+
+def find_dataset_types(search_datasets: Iterable[uuid.UUID]) -> dict[uuid.UUID, str]:
+    datasets = set(id.bytes for id in search_datasets)
+    root = "dp1-dump/datasets"
+    output = {}
+    for file in glob("*", root_dir=root):
+        dataset_type_name = file
+        path = os.path.join(root, file)
+        id_column = ParquetFile(path).read(columns=["dataset_id"]).column("dataset_id")
+        ids_for_this_type = set(id_column.to_pylist())
+        found = datasets.intersection(ids_for_this_type)
+        datasets = datasets - found
+        for id in found:
+            output[uuid.UUID(bytes=id)] = dataset_type_name
+
+    if datasets:
+        print(f"No dataset type found for some datasets: {[uuid.UUID(bytes=id) for id in datasets]}")
+
+    return output
 
 
 class FileAccess(NamedTuple):
